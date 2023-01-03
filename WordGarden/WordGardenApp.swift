c12f9f2af704a949e1a2e6d1632c1593d6322868
Jeremy Taylor <jtaylor@jctaylor.org>
2023-01-02 18:06:34 -0600

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Jeremy Taylor on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
